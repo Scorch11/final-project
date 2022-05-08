4e9de98e578d6f0e8ed0ff13a6f3d46a943c5cc3
Scorch11 <97859475+Scorch11@users.noreply.github.com>
2022-05-07 21:47:08 -0700

    Update mycode.py

--- mycode.py
+++ mycode.py
@@ -43,6 +43,19 @@ print(rule)
 counts = collect_results(rule, max_results=500, result_stream_args=enterprise_search_args)
 [print(c) for c in counts];
 
+#make into a readme.sh
+jupyter-nbconvert ./api_example.ipynb --to rst
+#jupyter-nbconvert ./credential_handling.ipynb --to rst
+
+sed 's/.*parsed-literal.*/::/' api_example.rst > _api_example.rst
+#sed 's/.*parsed-literal.*/::/' credential_handling.rst > _credential_handling.rst
+
+mv _api_example.rst api_example.rst
+#mv _credential_handling.rst credential_handling.rst
+
+$(brew --prefix)/bin/pandoc -i base_readme.rst --to rst | sed 's/ipython3/python/' > ../README.rst
+#| sed 's/.*parsed-literal.*/::/' 
+
 
 
 
