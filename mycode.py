de1a0d3dfa80dcdd05445f6d8caa666aa57bfd7a
Scorch11 <97859475+Scorch11@users.noreply.github.com>
2022-05-07 21:40:17 -0700

    Create mycode.py

---
+++ mycode.py
@@ -0,0 +1,58 @@
+enterprise_search_args = load_credentials("~/.twitter_keys.yaml",
+                                          yaml_key="search_tweets_enterprise",
+                                          env_overwrite=False)
+#option one
+from searchtweets import collect_results
+weets = collect_results(rule,
+                         max_results=100,
+                         result_stream_args=enterprise_search_args)
+[print(tweet.all_text, end='\n\n') for tweet in tweets[0:10]];
+# commented out, unless you want this data[print(tweet.created_at_datetime) for tweet in tweets[0:10]];
+[print(tweet.generator.get("name")) for tweet in tweets[0:10]];
+
+#option two
+rs = ResultStream(rule_payload=rule,
+                  max_results=500,
+                  max_pages=1,
+                  **premium_search_args)
+
+print(rs)
+tweets = list(rs.stream())
+# using unidecode to prevent emoji/accents printing 
+[print(tweet.all_text) for tweet in tweets[0:10]];
+count_rule = gen_rule_payload("beyonce", count_bucket="day")
+
+counts = collect_results(count_rule, result_stream_args=enterprise_search_args)
+counts
+
+#option three, shound like we should go this route due to the idea we will be able to do a full archive search
+rule = gen_rule_payload("from:jack",
+                        from_date="2017-09-01", #UTC 2017-09-01 00:00
+                        to_date="2017-10-30",#UTC 2017-10-30 00:00
+                        results_per_call=500)
+print(rule)
+tweets = collect_results(rule, max_results=500, result_stream_args=enterprise_search_args)
+[print(tweet.all_text) for tweet in tweets[0:10]];
+#modify below text for filters and ways to sort data
+rule = gen_rule_payload("from:jack",
+                        from_date="2017-09-20",
+                        to_date="2017-10-30",
+                        count_bucket="day",
+                        results_per_call=500)
+print(rule)
+counts = collect_results(rule, max_results=500, result_stream_args=enterprise_search_args)
+[print(c) for c in counts];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
